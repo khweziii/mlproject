353c3a3b18c3ebf93bc4ff574cf96ee7360c1658
khwezikgalema <kgalemakhwezi0@gmail.com>
2025-11-15 22:32:04 +0200

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,31 @@
+import sys 
+import logging , logger
+
+
+logger.logging.basicConfig(
+    filename=logger.LOG_FILE,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+def error_message_detail(error, error_detail: sys):
+    _,_,exc_info = error_detail.exc_info() # details about error will be stored in this variable
+    file_name = exc_info.tb_frame.f_code.co_filename
+    line_no = exc_info.tb_lineno
+    error_message = (
+        "Error occured in python script name [{0}] line number [{1}] error message [{2}]"
+        .format(file_name, line_no, str(error)))
+    file_name, exc_info.tb_lineno,str(error)
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
