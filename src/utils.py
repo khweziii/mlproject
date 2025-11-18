ec46a1858966f490fdf2fb1065347747886592db
khwezikgalema <kgalemakhwezi0@gmail.com>
2025-11-18 12:46:56 +0200

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd 
 import dill 
 from src.exception import CustomException
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
 
 def save_object(file_path, obj):
@@ -16,5 +17,39 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj: # write byte mode
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, X_test, y_train, y_test, models):
+    try:
+       
+        # X_train, X_test, y_train, y_test = train_test_split(
+        #         X, y, test_size=0.2, random_state=42
+        # )
+
+        report = {}
+
+        for i in range(len(list(models))):
+            '''
+            For each model, you will train it, evaluate it and store performance in 
+            report dictionary
+            '''
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train) # Train model
+
+            y_train_pred = model.predict(X_train) # evaluate model on train data
+
+            y_test_pred = model.predict(X_test) # evaluate model on test data
+
+            train_model_score = r2_score(y_train, y_train_pred) # calculate r2 on train data
+
+            test_model_score = r2_score(y_test, y_test_pred) # calculate r2 on test data
+
+            report[list(models.keys())[i]] = test_model_score # save model performance to report dictionary
+
+            return report 
+        
     except Exception as e:
         raise CustomException(e, sys)
